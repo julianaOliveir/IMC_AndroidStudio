c9f7a575a09a95d6f5f4fb78e3600cfe9c01e8a3
Juliana Oliveira <jullianaoliveira.2003@gmail.com>
2021-10-18 16:19:19 -0300

    Dashboard finalizado

---
+++ app/src/main/java/com/example/imc_app/DashBoardActivity.kt
@@ -0,0 +1,13 @@
+package com.example.imc_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+
+        supportActionBar!!.hide()
+    }
+}
--- app/src/main/java/com/example/imc_app/MainActivity.kt
+++ app/src/main/java/com/example/imc_app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.imc_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+
+        val buttonEntrar = findViewById<Button>(R.id.button_entrar)
+
+        buttonEntrar.setOnClickListener {
+            val abrirDashBoardActivity = Intent(this, DashBoardActivity::class.java)
+            startActivity(abrirDashBoardActivity)
+        }
     }
 }
